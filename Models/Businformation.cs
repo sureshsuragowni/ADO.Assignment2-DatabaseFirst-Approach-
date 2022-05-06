f2843b229e67915a8c0967c2a2f1bef551f18bff
Suragowni <suresh-gowd.suragowni@capgemini.com>
2022-05-06 10:29:52 +0530

    commited

---
+++ Models/Businformation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusInfo.Models
+{
+    public class Businformation
+    {
+        public int BusID { get; set; }
+        public string BoardingPoint { get; set; }
+        public Nullable<System.DateTime> TravelDate { get; set; }
+        public Nullable<decimal> Amount { get; set; }
+        public Nullable<int> Rating { get; set; }
+    }
+}
